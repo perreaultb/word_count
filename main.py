36d49650c5c4cbdf6d2890a28f494c4c9ca43a3c
Nullvain <perreaultb@educ.college-montreal.qc.ca>
2026-09-08 19:33:25 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+
+# compte le # de mots dans un string en comptant les nombres d`espaces
+def count_word(text: str):
+    word_count = 0 # nombre de mots
+    in_word = False # pour que 2 espaces ne comptent pas comme 2 mots etc etc
+
+    for letter in text:
+        if letter != " ":
+            if not in_word: 
+                word_count += 1
+                in_word = True # assure que plus que 1 espaces n'est pas confus comme plus que 1 mot
+        else:
+            in_word = False
+
+    return word_count
+
+
+print("il y a " + str(count_word("des    mots   des  mots des     mots")) + " mots dans cette phrase")
+
